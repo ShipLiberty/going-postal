8e0102e5046f3e7f7258546c5b3de47af59bcbe2
Jesse Rosalia <jesse@rosalia.me>
2017-02-27 13:52:34 -0800

    Added request logging

--- server/src/going_postal/handler.clj
+++ server/src/going_postal/handler.clj
@@ -18,8 +18,22 @@
   (route/resources "/")
   (route/not-found "Not Found"))
 
+(defn mywrap-request-logging [handler]
+  (fn [{:keys [request-method uri] :as req}]
+    (let [body (when (:body req)
+                     (slurp (:body req))
+                     (.reset (:body req)))
+          _ (println (format "Processing %s %s" (.toUpperCase (name request-method)) uri))
+          _ (println (format "\t Params: %s" req));; (:params req)))
+
+;          _ (println (format "\t Body: %s" body))
+          ;resp (if (:body req) (handler (assoc req :body (java.io.ByteArrayInputStream. (.getBytes body)))) (handler req))]
+          resp (if (:body req) (handler req) (handler req))]
+      (println (format "Response %s" resp))
+      resp)))
 (def app
   (-> (routes home-routes app-routes)
+      (mywrap-request-logging)
       (wrap-cors :access-control-allow-origin [#"http://localhost:3000"]
                  :access-control-allow-methods [:get :post])
       (handler/site)
