eb70357fdd197e92d67f89548544a9e9bf7ef905
Nate Dawg <Nathan.levine@gmail.com>
2017-04-02 14:03:47 -0500

    moved the search logic from app component into search component. still need to figure out how to move that information into the selection component

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit, Inject }          from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Http, URLSearchParams }              from '@angular/http';
-import { SelectComponent }                    from './search/search.component';
-import { SelectComponent }                    from './select/select.component';
-import { HeaderComponent }                    from './header/header.component';
-import { FooterComponent }                    from './footer/footer.component';
-import 'rxjs/add/operator/map';
-import { APP_CONFIG, IAppConfig }             from './app.config';
+import { Component }        from '@angular/core';
+import { SearchComponent }  from './search/search.component';
+import { SelectComponent }  from './select/select.component';
+import { HeaderComponent }  from './header/header.component';
+import { FooterComponent }  from './footer/footer.component';
 
 @Component({
   selector   : 'my-app',
@@ -24,51 +20,6 @@ export class AppComponent  {
         state    : '',
         zip      : 0
     };
-    
-    //called first time before the ngOnInit()    
-    constructor(private http: Http, 
-                @Inject(APP_CONFIG) private config: IAppConfig) {}
-    
-    /*
-    //called after the constructor and called  after the first ngOnChanges() 
-    ngOnInit(){
-
-    }*/
-    
-    //form stuff, includes setting the properties and the validation
-    form = new FormGroup({
-        street : new FormControl('', Validators.required),
-        city   : new FormControl('', Validators.required),
-        state  : new FormControl('', Validators.required),
-        zipcode: new FormControl('', Validators.compose([
-            Validators.required,
-            Validators.pattern('^\\d{5}(?:[-\\s]\\d{4})?$')
-        ])),
-    });
-
-    //function called on (form) submit
-    onSubmit() {
-    
-        //set the sender object details
-        this.sender.address = this.form.controls['street'].value;
-        this.sender.city    = this.form.controls['city'].value;
-        this.sender.state   = this.form.controls['state'].value;
-        this.sender.zip     = this.form.controls['zipcode'].value;
-        
-        //lookup GET request endpoint string buildup
-        var address = 'address=' + this.sender.address;
-        var city    = '&city='    + this.sender.city;
-        var state   = '&state='   + this.sender.state;
-        var zip     = '&zip='     + this.sender.zip;
-    
-        var getString = this.config.apiEndpoint + 'v1/lookup?' + address + city + state + zip;
-    
-        //GET request for the representatives
-        this.http.get(getString).subscribe(response => {
-                this.reps = response.json();
-                console.log(this.reps);
-        })
-    }
 }
 
 
--- website/src/app/search/search.component.ts
+++ website/src/app/search/search.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, Input }   from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Http, URLSearchParams }              from '@angular/http';
+import 'rxjs/add/operator/map';
+import { APP_CONFIG, IAppConfig }             from './../app.config';
 
 @Component({
   selector   : 'search-reps',
@@ -7,5 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class SearchComponent  {
+    //just some properties
+    @Input() reps  : any;
+    @Input() sender: any;
     
+    //called first time before the ngOnInit()    
+    constructor(private http: Http, 
+                @Inject(APP_CONFIG) private config: IAppConfig) {}
+    
+    //form stuff, includes setting the properties and the validation
+    form = new FormGroup({
+        street : new FormControl('', Validators.required),
+        city   : new FormControl('', Validators.required),
+        state  : new FormControl('', Validators.required),
+        zipcode: new FormControl('', Validators.compose([
+            Validators.required,
+            Validators.pattern('^\\d{5}(?:[-\\s]\\d{4})?$')
+        ])),
+    });
+
+    //function called on (form) submit
+    onSubmit() {
+    
+        //set the sender object details
+        this.sender.address = this.form.controls['street'].value;
+        this.sender.city    = this.form.controls['city'].value;
+        this.sender.state   = this.form.controls['state'].value;
+        this.sender.zip     = this.form.controls['zipcode'].value;
+        
+        //lookup GET request endpoint string buildup
+        var address = 'address=' + this.sender.address;
+        var city    = '&city='    + this.sender.city;
+        var state   = '&state='   + this.sender.state;
+        var zip     = '&zip='     + this.sender.zip;
+    
+        var getString = this.config.apiEndpoint + 'v1/lookup?' + address + city + state + zip;
+    
+        console.log(getString);
+        //GET request for the representatives
+        this.http.get(getString).subscribe(response => {
+                this.reps = response.json();
+                console.log(this.reps);
+        })
+    }
 }
