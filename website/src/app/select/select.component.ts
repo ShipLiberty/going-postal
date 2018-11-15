63a869b9db0a9205d215008648291c83550aaad3
Nate Dawg <Nathan.levine@gmail.com>
2018-11-15 17:37:03 -0500

    working on multiple selection of reps, got the select component changes (multiple selction, style changes, data structure changes) in. Next got to get teh array over and change the letter component to handle the array rather then the individual rep, #5

--- website/src/app/select/select.component.ts
+++ website/src/app/select/select.component.ts
@@ -9,18 +9,101 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 export class SelectComponent  {
     //just some properties
-    @Input() representatives:any;
-    
+    @Input() representatives:any; //represnetatives that are shown, input from the search component
+    selectedReps: Array<Custom> = [];
+
+    @Output() selectedRepresentatives: EventEmitter<any> = new EventEmitter<any>();
     @Output() next: EventEmitter<any> = new EventEmitter<any>();
-    @Output() repSelected  : EventEmitter<any> = new EventEmitter<any>();
-        
+
+    //individual select button statuses
+    status0: boolean = false;
+    status1: boolean = false;
+    status2: boolean = false;
+
+    //individual seleect button text
+    selectButtonText0: string = "select";
+    selectButtonText1: string = "select";
+    selectButtonText2: string = "select";
+
     //called when a representative is selected
-    onRepClick(rep) {
-        //notify app component that a rep was selected
-        this.repSelected.emit(rep);
-        this.next.emit();
-        
-        //log rep (for debugging)
-        console.log(JSON.stringify(rep, null, 4));
+    onRepClick(rep, i) {
+      //add or take rep out of array of selected reps
+      if (this.selectedReps.indexOf(rep) > -1) {
+        this.selectedReps = this.selectedReps.filter(obj => obj !== rep);
+      } else {
+        this.selectedReps.push(rep);
+      }
+
+      //change button style/text
+      if (i == 0) {
+        this.status0 = !this.status0;
+        this.selectButtonText0 = (this.selectButtonText0 == "select") ? "deselect" : "select";
+      } else if (i == 1) {
+        this.status1 = !this.status1;
+        this.selectButtonText1 = (this.selectButtonText1 == "select") ? "deselect" : "select";
+      } else if (i == 2) {
+        this.status2 = !this.status2;
+        this.selectButtonText2 = (this.selectButtonText2 == "select") ? "deselect" : "select";
+      }
+
+      //log rep (for debugging)
+      //console.log("Reps selcted: " + JSON.stringify(this.selectedReps, null, 4));
+    }
+
+    getText(i) {
+      if (i == 0) {
+        return this.selectButtonText0;
+      } else if (i == 1) {
+        return this.selectButtonText1;
+      } else if (i == 2) {
+        return this.selectButtonText2;
+      } else {
+        //shouldn't hit
+        return "select";
+      }
+    }
+
+    //a helper method to get the class for the select/deselect button in rep cards
+    getButtonClasses(i) {
+      if (i == 0) {
+        if (this.status0 == true) {
+          return 'ui green button';
+        } else {
+          return 'ui basic green button';
+        }
+      }
+
+      if (i == 1) {
+        if (this.status1 == true) {
+          return 'ui green button';
+        } else {
+          return 'ui basic green button';
+        }
+      }
+
+      if (i == 2) {
+        if (this.status2 == true) {
+          return 'ui green button';
+        } else {
+          return 'ui basic green button';
+        }
+      }
+    }
+
+    //called when the next button is clicked
+    onNextClick(reps) {
+      console.log("next button clicked");
+      //notify app component that a rep was selected
+      this.selectedRepresentatives.emit(reps);
+      this.next.emit();
+    }
+
+    //a helper method to determine if the next button should be disabled or not
+    checkSelectedReps() {
+      if (this.selectedReps.length > 0) {
+        return false;
+      } else {
+        return true;
+      }
     }
 }
