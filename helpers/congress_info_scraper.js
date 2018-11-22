87283dee733ee1e938eaadf7a499e8584b325ba5
Nate Dawg <Nathan.levine@gmail.com>
2018-11-21 21:02:33 -0500

    added a helpers folder with two scrapers (pretttty similar so might want to condense into one scraper) for representative and senate info(image url included) scraping

---
+++ helpers/congress_info_scraper.js
@@ -0,0 +1,50 @@
+const puppeteer = require('puppeteer');											//setup the chrome library for scraping			
+
+const houseURL = 'https://vote-usa.org/officials.aspx?report=u3';
+
+(async () => {
+	const browser = await puppeteer.launch({
+	headless: false, 															// default is true, this lets us see in action
+	//slowMo: 500 																// slow down by 500ms
+	});
+	const page = await browser.newPage();										
+	await page.goto(houseURL);
+	let arrayOfReps = await page.evaluate(() => {  								
+		let repsArray = [];  													
+		let repElements = Array.from(document.querySelectorAll('div.candidate-cell'));   
+		repElements.forEach((repElement) => { 											
+			let repData = {};
+			try {
+				//office
+				repData.office = 'congressperson';
+				//get district
+                repData.district = repElement.querySelector('div.cell-heading').innerText;
+                
+                //get name and party intial
+                let nameAndParty = repElement.querySelector('div.candidate-name span').innerText;
+                let nameAndPartySplitArray = nameAndParty.split('-');
+                let name = nameAndPartySplitArray[0];
+                repData.name = name;
+                let partyInitial = nameAndPartySplitArray[1];
+                repData.partyInitial = partyInitial;
+                
+                //get image url
+                let repDateImageHTML = repElement.querySelector('div.candidate-image').innerHTML;
+                let imageURLSplitArray = repDateImageHTML.split('"');
+                repData.imageURL = 'https://vote-usa.org' + imageURLSplitArray[1];
+
+                //get age
+                repData.age = repElement.querySelector('div.candidate-age').innerText;
+                
+                //get website
+                repData.website = repElement.querySelector('div.candidate-website span a').getAttribute("href");
+            }
+            catch (exception){
+            }
+			repsArray.push(repData); 									
+		}); 														
+		return repsArray;
+	});
+	console.log(arrayOfReps);
+	await browser.close();												
+})();
